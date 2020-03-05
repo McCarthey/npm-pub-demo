67f8c3b4f46cd114b189611fd38c0aee811af3de
Randall <randall@duomai.com>
2020-03-05 11:53:21 +0800

    [test] export {XXX} or export default

---
+++ print.js
@@ -0,0 +1,3 @@
+export default function print(string) {
+    console.log(string)
+}
--- webpack.config.js
+++ webpack.config.js
@@ -3,32 +3,36 @@ const path = require('path')
 const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
-    mode: 'production',
+    // mode: 'production',
+    mode: 'development',
     entry: {
         bundle: './index.js'
     },
     output: {
         filename: "index.js",
+        libraryTarget: 'umd',
         path: path.resolve(__dirname, 'lib')
     },
     resolve: {
         extensions: ['.js']  //用于配置程序可以自行补全哪些文件后缀
     },
-    optimization: {
-        minimize: true,
-        minimizer: [new TerserPlugin()],
-    },
-    // plugins: [
-    //     new UglifyJsPlugin({
-    //         uglifyOptions: {
-    //             compress: {
-    //                 drop_console: true,
-    //                 dead_code: true,    //移除没被引用的代码
-    //                 warnings: false,    //当删除没有用处的代码时，显示警告
-    //                 loops: true         //当do、while 、 for循环的判断条件可以确定是，对其进行优化
-    //             }
-    //         },
-    //         sourceMap: false
-    //     })
-    // ]
+    // optimization: {
+    //     minimize: true,
+    //     minimizer: [new TerserPlugin()],
+    // },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,      // 匹配js文件，然后用下面所配置的工具对这些文件进行编译处理
+                use: {
+                    loader: 'babel-loader',     // babel的核心模块
+                    options: {
+                        presets: [              // 配置babel的预设，将ES语法转成ES5语法
+                            '@babel/preset-env'
+                        ]
+                    }
+                }
+            }
+        ]
+    }
 };
